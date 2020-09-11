500960c7a60e477c6e3c4ee16e73915587a5c36d
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-11 15:13:44 -0600

    Created components from template code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+// Import Components
+import Form from './components/Form';
+import ToDoList from './components/ToDoList';
+
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
     <header>
       <h1>Fahad's Todo List</h1>
     </header>
+    <Form />
+    <ToDoList />
     </div>
   );
 }
